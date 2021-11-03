7fc3a0e84623b5c5c13d204284076cb247a8e604
sujeong_BPNR <sujeong@bpnr.co.kr>
2021-11-03 11:55:25 +0900

    scss추가 npm i sass-loader@10, lang="scss" , CustomCheckBox.vue 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    a
+    <CustomCheckBox/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CustomCheckBox from './components/CustomCheckBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CustomCheckBox
   }
 }
 </script>
---
+++ src/components/CustomCheckBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="custom-check-box" :class="{checked:checked}">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomCheckBox',
+  data(){
+    return {
+      checked: false
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.custom-check-box {
+  .checked {
+
+  }
+}
+</style>
